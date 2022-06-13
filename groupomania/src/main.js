946c06eac1a8953ed46a09fb3c3959ea528a584f
jda447 <user.email>
2022-06-13 19:55:38 +0100

    fontawesome

--- groupomania/src/main.js
+++ groupomania/src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPen);
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
