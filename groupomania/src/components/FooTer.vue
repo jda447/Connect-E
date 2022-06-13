ffd4f050cc6b01a526f050322e60706e6d4d8034
jda447 <user.email>
2022-06-13 23:22:59 +0100

    FooTer added

---
+++ groupomania/src/components/FooTer.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+	name: 'FooTer'
+}
+</script>
+
+<template>
+    <footer class="footer text-center mt-5 p-3">
+      © 2022 Copyright:
+      <a class="text-reset fw-bold" href="#">Groupomania.com</a>
+    </footer>
+</template>
+
+<style>
+.footer {
+    background-color: rgba(0, 0, 0, 0.1);
+    bottom: 0;
+}
+</style>
