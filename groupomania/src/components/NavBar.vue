68f93b1c88832fcf4e62fe2189d6f34e8ba27ce5
jda447 <user.email>
2022-06-14 15:18:12 +0100

    profileBtn in navbar

--- groupomania/src/components/NavBar.vue
+++ groupomania/src/components/NavBar.vue
@@ -1,5 +1,7 @@
 <template>
   <nav class="navbar navbar-light bg-danger mb-3">
+    <div class="btn btn-danger p-2 mx-2 col-1">
+    <font-awesome-icon :icon="['fas', 'user-large']" /></div>
   <a class="navbar-brand mx-auto" href="#">Groupomania</a></nav>
 </template>
 
