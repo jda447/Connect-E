d6e2121b1fc5d47d109fda0b84eeb0cc44e7e9fb
jda447 <user.email>
2022-06-14 15:18:12 +0100

    profileBtn in navbar

--- groupomania/src/App.vue
+++ groupomania/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <NavBar></NavBar>
-    <ProfileBtn></ProfileBtn>
     <form @submit.prevent= 'createPost'>
     <div><label class="m-2">Write something... <font-awesome-icon :icon="['fas', 'pencil']" /></label></div>
     <div><textarea v-model="body"></textarea></div>
@@ -17,16 +16,13 @@
 
 <script>
 import NavBar from './components/NavBar.vue'
-import ProfileBtn from './components/ProfileBtn.vue'
 import UserPosts from './components/UserPosts.vue'
 import FooTer from './components/FooTer.vue'
-
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    ProfileBtn,
     UserPosts,
     FooTer
   },
@@ -54,8 +50,10 @@ export default {
 <style>
 #app {
   font-family: Helvetica, Arial, sans-serif;
-  text-align: center;
   color: #2c3e50;
   border: solid 10px white;
+}
+form {
+  text-align: center
 }
 </style>
