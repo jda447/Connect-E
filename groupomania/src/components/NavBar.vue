4cf99e52de31b2416c076be20cc6cd839c0b767a
jda447 <user.email>
2022-06-14 16:10:11 +0100

    sign-out btn

--- groupomania/src/components/NavBar.vue
+++ groupomania/src/components/NavBar.vue
@@ -1,8 +1,10 @@
 <template>
   <nav class="navbar navbar-light bg-danger mb-3">
     <div class="btn btn-danger p-2 mx-2 col-1">
-    <font-awesome-icon :icon="['fas', 'user-large']" /></div>
-  <a class="navbar-brand mx-auto" href="#">Groupomania</a></nav>
+      <font-awesome-icon :icon="['fas', 'user-large']" /></div>
+    <a class="navbar-brand mx-auto" href="#">Groupomania</a>
+      <div class="btn btn-danger mx-1 col-1">
+      <font-awesome-icon :icon="['fas', 'right-from-bracket']" /></div></nav>
 </template>
 
 <script>
--- groupomania/src/main.js
+++ groupomania/src/main.js
@@ -7,10 +7,12 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import { faUserLarge } from "@fortawesome/free-solid-svg-icons";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faPencil);
 library.add(faUserLarge);
 library.add(faMessage);
+library.add(faRightFromBracket);
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
