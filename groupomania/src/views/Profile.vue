3fecbecbfae04832b45861b681c010b830d618f7
jda447 <user.email>
2022-06-16 23:02:14 +0100

    Sign/log-in pages added

---
+++ groupomania/src/views/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ProfileNav></ProfileNav>
+    <div><label class="m2">Profile Picture:</label></div>
+    <div><label class="m2">User Name:</label></div>
+    <div><label class="m2">Company Position:</label></div>
+    <div><label class="m2"></label></div>
+    <FooTer></FooTer>
+  </div>
+</template>
+
+<script>
+import ProfileNav from '../components/ProfileNav.vue'
+import FooTer from '../components/FooTer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProfileNav,
+    FooTer
+  }
+}
+</script>
+
+<style>
+  .dot {
+    border-radius: 50%;
+  }
+</style>
---
+++ groupomania/src/views/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    Sign-in
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
