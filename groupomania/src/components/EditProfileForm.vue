6697567a04bec5ead5c3149912e8490dda3852df
jda447 <user.email>
2022-06-19 01:14:34 +0100

    EditProfileForm component

---
+++ groupomania/src/components/EditProfileForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mb-2"><label>Edit Profile Picture: </label>
+    <input type="file" @change="onFileSelected">
+    </div>
+    <div class="mb-2"><label>Edit User Name: </label>
+    <input type="text" @change="onFileSelected">
+    </div>
+    <div class="mb-2"><label>Edit Company: </label>
+    <input type="text" @change="onFileSelected">
+    </div>
+    <button @click="onUpload" class="btn btn-success mt-4">Upload</button>
+</template>
+
+<script>
+export default {
+	name: 'EditProfileForm'
+}
+</script>
+
+<style>
+
+</style>
---
+++ groupomania/src/views/EditProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ProfileNav></ProfileNav>
+    <div><label class="m2">Edit Profile</label></div>
+    <FooTer></FooTer>
+  </div>
+</template>
+
+<script>
+import ProfileNav from '../components/ProfileNav.vue'
+import FooTer from '../components/FooTer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProfileNav,
+    FooTer
+  }
+}
+</script>
+
+<style>
+
+</style>
