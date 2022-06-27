a1422e5faec94341531e6225c98debf583a17bab
jda447 <user.email>
2022-06-27 10:41:35 +0100

    mess/prof styling

--- groupomania/src/components/HomeNav.vue
+++ groupomania/src/components/HomeNav.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav class="navbar navbar-light bg-danger mb-3">
-    <router-link to="/profile" class="btn btn-danger p-2 mx-3 col-1">
+  <nav class="navbar navbar-light bg-danger navbarGradient mb-3">
+    <router-link to="/profile" class="btn btn-danger px-3 mx-3">
       <font-awesome-icon :icon="['fas', 'user-large']" />
     </router-link>
-    <a class="navbar-brand mx-auto" href="#">Groupomania</a>
-      <div class="btn btn-danger mx-3 col-1">
+    <a class="navbar-brand mx-auto" href="#">Messages</a>
+      <div class="btn btn-danger mx-3">
       <font-awesome-icon :icon="['fas', 'right-from-bracket']" />
     </div>
   </nav>
@@ -16,8 +16,13 @@ export default {
 }
 </script>
 
-<style>
-  .navbar {
-    font-weight: bold;
+<style scoped>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  border: solid 10px white;
+}
+.navbarGradient {
+  background-image: linear-gradient(to bottom, #d9534f, white);
   }
 </style>
