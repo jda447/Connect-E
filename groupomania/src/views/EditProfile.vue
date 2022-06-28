91175516f959933947d84efd4036d25d5363e62e
jda447 <user.email>
2022-06-28 13:36:21 +0100

    file upload works

--- groupomania/src/views/EditProfile.vue
+++ groupomania/src/views/EditProfile.vue
@@ -1,22 +1,47 @@
 <template>
   <div id="app">
   <EditProfileNav></EditProfileNav>
-  <EditProfileForm></EditProfileForm>
+  <div>
+    <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectImage"> </div>
+    <input ref="fileInput" type="file" @input="pickFile">
+  </div>
   <FooTer></FooTer>
   </div>
 </template>
 
 <script>
 import EditProfileNav from '../components/EditProfileNav.vue'
+// import EditProfileForm from '../components/EditProfileForm.vue'
 import FooTer from '../components/FooTer.vue'
-import EditProfileForm from '../components/EditProfileForm.vue'
 
 export default {
   name: 'App',
   components: {
     EditProfileNav,
-    EditProfileForm,
+    // EditProfileForm,
     FooTer
+  },
+  data() {
+      return {
+        previewImage: null
+      };
+    },
+  methods: {
+      selectImage () {
+          this.$refs.fileInput.click()
+      },
+      pickFile () {
+        let input = this.$refs.fileInput
+        let file = input.files
+        if (file && file[0]) {
+          let reader = new FileReader
+          reader.onload = e => {
+            this.previewImage = e.target.result
+          }
+          reader.readAsDataURL(file[0])
+          this.$emit('input', file[0])
+        }
+      }
   }
 }
 </script>
@@ -27,5 +52,14 @@ export default {
   color: #2c3e50;
   border: solid 10px white;
   font-family: Pacifico;
+}
+.imagePreviewWrapper {
+    width: 250px;
+    height: 250px;
+    display: block;
+    cursor: pointer;
+    margin: 0 auto 30px;
+    background-size: cover;
+    background-position: center center;
 }
 </style>
