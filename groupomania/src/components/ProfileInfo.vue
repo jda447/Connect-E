3715dfb4ed577702d9f03d2701d956e167d6c7e6
jda447 <user.email>
2022-07-18 16:00:55 +0100

    form renders & footer deleted

---
+++ groupomania/src/components/ProfileInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <div class="text-center labels">
+        <div v-for="(review, index) in reviews" :key="index"><label class="my-2">
+            Name: <span class="userFont"> {{ review.name }} </span></label></div>
+        <div v-for="(review, index) in reviews" :key="index"><label class="my-2">
+            Position: <span class="userFont"> {{ review.review }} </span></label></div>
+        <div v-for="(review, index) in reviews" :key="index"><label class="my-2">
+            Hobbies: <span class="userFont"> {{ review.rating }} </span></label></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+    props: {
+    reviews: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
--- groupomania/src/views/Profile.vue
+++ groupomania/src/views/Profile.vue
@@ -1,75 +1,39 @@
 <template>
   <div id="app">
     <ProfileNav></ProfileNav>
-    <div class="counter text-center m-3 p-4">
-      {{ $store.state.counter }}
-    </div>
-    <div class="buttons text-center">
-      <button @click="$store.dispatch('increaseCounter')" class="btn btn-success mx-3">+</button>
-      <button @click="$store.dispatch('decreaseCounter')" class="btn btn-danger mx-3">-</button>
-    </div>
-      <!-- <div class="mx-auto col-4 col-md-4 col-lg-2 my-1">
-        <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectImage"> </div>
-      </div>
-      <div class="text-center labels">
-        <div><label class="my-2">Name: <span class="userFont"> {{ userName }} </span></label></div>
-        <div><label class="my-2">Position: <span class="userFont"> {{ userPosition }} </span></label></div>
-        <div><label class="my-2">Hobbies: <span class="userFont"> {{ userHobbies }} </span></label></div>
-      </div>
+    <ProfileInfo :reviews="reviews"></ProfileInfo>
       <div class="text-center">
         <router-link to="/edit-profile" class="btn btn-primary btn-lg mt-3">Edit</router-link>
-      </div> -->
-    <FooTer></FooTer>
+      </div>
   </div>
 </template>
 
 <script>
 import ProfileNav from '../components/ProfileNav.vue'
-import FooTer from '../components/FooTer.vue'
+import ProfileInfo from '../components/ProfileInfo.vue'
 
 export default {
   name: 'App',
   components: {
     ProfileNav,
-    FooTer
+    ProfileInfo
   },
-  // data() {
-  //   return {
-  //     // userProfile: true,
-  //     // previewImage: null,
-  //     // message: '',
-  //     counter: 0
-  //   }
-  // },
-  // methods: {
-  //   increaseCounter() {
-  //     // this.$store.commit('increaseCounter')
-  //     this.counter++
-  //     console.log(this.$store.state.counter)
-  //   },
-  //   decreaseCounter() {
-  //     this.counter--
-  //   }
-  // }
-  //   toggle() {
-  //     this.userProfile = !this.userProfile
-  //   },
-  //   selectImage () {
-  //     this.$refs.fileInput.click()
-  //   },
-  //   pickFile () {
-  //     let input = this.$refs.fileInput
-  //     let file = input.files
-  //     if (file && file[0]) {
-  //       let reader = new FileReader
-  //       reader.onload = e => {
-  //         this.previewImage = e.target.result
-  //       }
-  //       reader.readAsDataURL(file[0])
-  //       this.$emit('input', file[0])
-  //     }
-  //   }
-  // }
+  data () {
+    return{
+      props: {
+        names: {
+          type: Array,
+          required: true
+        }
+      },
+    reviews: []
+    }
+  },
+  methods: {
+    addReview (review) {
+      this.reviews.push(review)
+    },
+  }
 }
 </script>
 
