3ab3e54a564ce101ccdea1afa21dc4b6ef9ef53a
jda447 <user.email>
2022-07-20 23:11:58 +0100

    nesting

--- groupomania/src/components/ProfileInfo.vue
+++ groupomania/src/components/ProfileInfo.vue
@@ -1,14 +1,17 @@
 <template>
     <div id="app">
       <div class="text-center labels">
-        <div v-for="(person, index) in info" :key="index"><label class="my-2">
-            Name: <span class="userFont"> {{ person.name }} </span></label>
+        <div v-for="(person, index) in info" :key="index">
+        <label class="my-2">
+            Name: {{ person.name }} </label>
         </div>
-        <div v-for="(person, index) in info" :key="index"><label class="my-2">
-            Position: <span class="userFont"> {{ person.position }} </span></label>
+        <div v-for="(person, index) in info" :key="index">
+        <label class="my-2">
+            Position: {{ person.position }} </label>
         </div>
-        <div v-for="(person, index) in info" :key="index"><label class="my-2">
-            Hobbies: <span class="userFont"> {{ person.hobbies }} </span></label>
+        <div v-for="(person, index) in info" :key="index">
+        <label class="my-2">
+            Hobbies: {{ person.hobbies }} </label>
         </div>
       </div>
     </div>
--- groupomania/src/store/index.js
+++ groupomania/src/store/index.js
@@ -3,22 +3,19 @@ import { createStore } from 'vuex'
 // Create a new store instance.
 export default createStore({
   state: {
-      counter: 0
+      name: '',
+      position: null,
+      hobbies: ''
   },
   mutations: {
-    increaseCounter(state) {
-      state.counter++
-    },
-    decreaseCounter(state) {
-      state.counter--
-    }
+
   },
   actions: {
-    increaseCounter (context) {
-      context.commit('increaseCounter')
-    },
-    decreaseCounter (context) {
-      context.commit('decreaseCounter')
+
+  },
+  getters: {
+    employeeDetails (state) {
+      return `${state.name}`
     }
   }
 })
