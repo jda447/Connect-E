1022de6e1c5c049c3600492012b3167afb4d402f
jda447 <user.email>
2022-07-26 22:45:20 +0100

    msg feed

--- groupomania/src/components/EditProfileForm.vue
+++ groupomania/src/components/EditProfileForm.vue
@@ -1,56 +1,56 @@
 <template>
   <div id="app">
-    <form class="editProfileForm" @submit.prevent="onSubmit">
-        <div class="mx-auto text-center col-4 col-md-4 col-lg-2 mt-2">
+    <form class="editProfileForm mx-auto" @submit.prevent="onSubmit">
+        <div class="mx-auto col-4 col-md-4 col-lg-2 mt-2">
           <input ref="fileInput" type="file" @input="pickFile">
         </div>
         <div class="mx-auto col-4 col-md-4 col-lg-2 my-2">
           <label>Name</label>
-          <input v-model="name" id="name" class="userData" type="text" placeholder="Add your name" required/>
+          <input v-model="name" id="name" type="text" placeholder="Add your name" required/>
         </div>
         <div class="mx-auto col-4 col-md-4 col-lg-2 my-2">
         <label>Position</label><br>
-        <select v-model="position" id="position" class="userData p-1" required>
-          <option class="userData" disabled value="">Add your position</option>
-          <option class="userData">Customer Service</option>
-          <option class="userData">Manager</option>
-          <option class="userData">CEO</option>
+        <select v-model="position" id="position" class="p-1" required>
+          <option disabled value="">Add your position</option>
+          <option>Customer Service</option>
+          <option>Manager</option>
+          <option>CEO</option>
         </select>
         </div>
         <div class="mx-auto col-4 col-md-4 col-lg-2 my-2">
           <label>Hobbies</label>
-          <input v-model="hobbies" id="hobbies" class="userData" placeholder="Add your hobbies" type="text" required>
+          <input v-model="hobbies" id="hobbies" placeholder="Add your hobbies" type="text" required>
         </div>
         <div class="mx-auto text-center col-4 col-md-4 col-lg-2 my-3">
-        <button v:on-click="onSubmit" class="btn btn-success px-3" type="submit" value="Submit">Upload</button>
+        <button v:on-click="onSubmit" class="btn btn-success px-3" type="submit">Upload</button>
         </div>
       </form>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'App',
-        data() {
-    return {
-        name: '',
-        position: null,
-        hobbies: ''
-      };
-    },
-    methods: {
-      onSubmit () {
-        let employeeInfo = {
-            name: this.name,
-            position: this.position,
-            hobbies: this.hobbies
-        }
-        this.$emit('info-submitted', employeeInfo)
-
-        this.name = ''
-        this.position = null
-        this.hobbies = ''
+export default {
+  name: 'App',
+    data() {
+  return {
+    name: '',
+    position: null,
+    hobbies: ''
+    };
+  },
+  methods: {
+    onSubmit () {
+      let employeeInfo = {
+        name: this.name,
+        position: this.position,
+        hobbies: this.hobbies
       }
+      this.$emit('info-submitted', employeeInfo)
+
+      this.name = ''
+      this.position = null
+      this.hobbies = ''
+    }
   }
 }
 </script>
--- groupomania/src/components/ProfileInfo.vue
+++ groupomania/src/components/ProfileInfo.vue
@@ -3,13 +3,9 @@
       <div class="text-center labels">
         <div v-for="(person, index) in info" :key="index">
         <label class="my-2">
-            Name: {{ person.name }} </label>
-        </div>
-        <div v-for="(person, index) in info" :key="index">
+            Name: {{ person.name }} </label><br>
         <label class="my-2">
-            Position: {{ person.position }} </label>
-        </div>
-        <div v-for="(person, index) in info" :key="index">
+            Position: {{ person.position }} </label><br>
         <label class="my-2">
             Hobbies: {{ person.hobbies }} </label>
         </div>
@@ -22,7 +18,7 @@ export default {
   name: 'App',
     props: {
     info: {
-      type: Array,
+      type: Object,
       required: true
     }
   }
