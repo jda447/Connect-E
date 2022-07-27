f14c40e985889a3902939c8e6206bfdd9c583bb8
jda447 <user.email>
2022-07-27 21:21:11 +0100

    employee details computes

--- groupomania/src/views/Profile.vue
+++ groupomania/src/views/Profile.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
     <ProfileNav></ProfileNav>
-    <ProfileInfo :info="info"></ProfileInfo>
-      <div class="text-center">
-        <p>{{ employeeDetails }}</p>
-        <router-link to="/edit-profile" class="btn btn-primary btn-lg mt-3">Edit</router-link>
-      </div>
+    <div class="text-center">
+      <ProfileInfo :info="info"></ProfileInfo>
+      <router-link to="/edit-profile" class="btn btn-primary btn-lg mt-3">Edit</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import ProfileNav from '../components/ProfileNav.vue'
 import ProfileInfo from '../components/ProfileInfo.vue'
-import { mapGetters } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     ProfileNav,
     ProfileInfo
-  },
-  methods: {
-    addUserName() {
-      this.$store.commit('ADD_NAME')
-    }
-  },
-  computed: {
-    ...mapGetters(['employeeDetails'])
   }
 }
 </script>
@@ -37,51 +27,16 @@ export default {
 #app {
   color: #2c3e50;
   border: solid 10px white;
-  font-family: Pacifico;
-}
-
-.labels {
-  color: #0275d8;
-}
-
-.userFont {
-  color: black;
-  font-size: larger;
-}
-
-.userData {
   font-family: Arial, Helvetica, sans-serif;
 }
 
 .btn-primary {
   background-color: #0275d8;
   color: white;
+  font-family: Pacifico;
   &:hover {
     background-color: white;
     color: #0275d8;
   }
-}
-
-// .btn-success {
-//   background-color: #4c9173;
-//   color: white;
-//   &:hover {
-//     background-color: white;
-//     color: #4c9173;
-//   }
-// }
-
-.imagePreviewWrapper {
-    width: 150px;
-    height: 150px;
-    display: block;
-    cursor: pointer;
-    margin: 0 auto;
-    background-size: cover;
-    background-position: center center;
-}
-
-.counter {
-  font-size: larger;
 }
 </style>
