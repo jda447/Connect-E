2c8428722fd44a2362b16a68c754681c567e1af7
jda447 <user.email>
2022-07-28 00:22:48 +0100

    messages render

--- groupomania/src/views/Messages.vue
+++ groupomania/src/views/Messages.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="app">
     <HomeNav></HomeNav>
-      <form @submit.prevent= 'createPost'>
+      <div class="create-new">
+        <input type="text"
+        class="px-2"
+        v-model="newPost"
+        placeholder="Add a post" 
+        @keypress.enter="addPost">
+        <button class="btn btn-success btn-sm mb-1 px-3" @click="addPost">Send</button>
+      </div>
+
+      <div class="posts">
+        <UserPosts
+          v-for="(post, i) in $store.state.posts"
+          :key="i"
+          :post="post">
+        </UserPosts>
+      </div>
+
+      <!-- <form @submit.prevent= 'createPost'>
         <div class="input-group mb-3">
           <input v-model="body" type="text" class="col ms-5 ps-2" aria-label="Message" aria-describedby="Message">
             <button class="btn btn-success me-5" type="submit">Send</button>
@@ -11,7 +28,7 @@
         <div v-if=post.body class="border shadow col mt-4 mb-2 px-3 pt-3 mx-5">
           <UserPosts :body="post.body"></UserPosts>
         </div>
-      </div>
+      </div> -->
   </div>
 </template>
 
@@ -25,25 +42,20 @@ export default {
     HomeNav,
     UserPosts
   },
-  data: () => ({
-    body: '',
-      posts: [
-        {
-          body: ''
-        }
-      ]
-    }),
-    methods: {
-      createPost () {
-        this.body
-
-        this.posts.unshift({
-          body: this.body
-        })
-        this.body = ''
+  data() {
+    return {
+      newPost: ''
+    }
+  },
+  methods: {
+    addPost () {
+      if (this.newPost) {
+        this.$store.commit('ADD_POST', this.newPost)
+        this.newPost = ''
       }
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -54,10 +66,6 @@ export default {
   color: #2c3e50;
   border: solid 10px white;
 }
-
-// .border {
-//   width: fit-content;
-// }
 
 .btn-success {
   font-family: Pacifico;
