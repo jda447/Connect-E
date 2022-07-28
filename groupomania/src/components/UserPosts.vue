e725868e75463f7c7a6087d6a99563aeb2ae31dc
jda447 <user.email>
2022-07-28 02:03:40 +0100

    userPosts details

--- groupomania/src/components/UserPosts.vue
+++ groupomania/src/components/UserPosts.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <div class="mx-auto">
+    <div class="mx-auto border shadow col-10 mb-3 mt-2">
+      <div @click="removePost" class="btn btn-outline btn-sm d-flex justify-content-end me-1">x
+      </div>
       <div :class="`post ${post.done ? 'is-complete' : ''}`">
-        <div class="content">{{ post.content }}</div>
-        <div @click="removePost" class="btn btn-danger btn-sm">x
-        </div>
+      <div class="content mx-4 p-2 flex-grow-1 bd-highlight">{User Name}<br><hr/>
+        {{ post.content }}</div>
+        <div class="mt-auto p-2 d-flex align-items-end flex-column bd-highlight">Seen by 0</div>
       </div>
     </div>
   </div>
@@ -22,3 +24,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.btn-outline {
+  font-weight: bolder;
+}
+</style>
