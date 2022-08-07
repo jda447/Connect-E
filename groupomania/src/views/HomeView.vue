0dc6311baef022535ed7abdfb73154054f65b029
jda447 <user.email>
2022-08-07 02:28:59 +0100

    homepage redesign

--- groupomania/src/views/HomeView.vue
+++ groupomania/src/views/HomeView.vue
@@ -2,14 +2,20 @@
   <div id="app">
     <div class="container">
       <div class="row">
-        <div class="col-xs-12">
-          <div  class="text-center">
-            <div class="mt-5"><img class="col-8 col-lg-4" src="@/assets/icon-left-font-monochrome-resized.png"/></div>
-              <router-link to="/sign-up" class="btn btn-outline-info btn-lg m-5">Sign-up</router-link>
-              <router-link to="/log-in" class="btn btn-outline-warning btn-lg m-5">Log-in</router-link>
+        <div  class="text-center">
+          <div class="my-5">
+            <img class="col-8 col-md-6 col-lg-4" src="@/assets/icon-above-font-cropped.png"/>
           </div>
+            <router-link to="/sign-up" class="btn btnSignUp btn-lg m-4">Sign-up</router-link>
+            <router-link to="/log-in" class="btn btn-outline btnLogIn  btn-lg m-4">Log-in</router-link>
         </div>
       </div>
+    </div>
+    <div class="footerWrapper">
+      <footer class="footer text-center mx-auto p-3">
+        © 2022 Copyright:
+        <a class="text-reset fw-bold" href="#">Groupomania.com</a>
+      </footer>
     </div>
   </div>
 </template>
@@ -20,22 +26,55 @@ export default {
 }
 </script>
 
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Ubuntu&display=swap');
 
 #app {
   font-family: Helvetica, Arial, sans-serif;
   color: #2c3e50;
-  border: solid 10px white;
-  min-height: 100vh;
-  background-image: linear-gradient(to bottom right, #2688e3, black, black, black, #fdc500);
+  border: solid 20px white;
+  height: 100vh;
+  background-image: linear-gradient(to bottom, white, white, white, white, white);
 }
 
-.btn-outline-info {
-  font-family: Pacifico;
+.btnSignUp {
+  font-family: Ubuntu, sans-serif;
+  background-color: #033270;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+    &:hover {
+    background-color: white;
+    color: #033270;
+    transform: scale(1.6);
+    transition: transform 250ms;
+  }
 }
 
-.btn-outline-warning {
-  font-family: Pacifico;
+.btnLogIn {
+  font-family: Ubuntu, sans-serif;
+  background-color: #033270;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+    &:hover {
+    background-color: white;
+    color: #033270;
+    transform: scale(1.6);
+    transition: transform 250ms;
+  }
+}
+
+// .footerWrapper {
+  
+// }
+
+.footer {
+  background-color: rgba(0, 0, 0, 0.1);
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 95%;
 }
 </style>
