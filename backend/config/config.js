27828fc9f6b91bb1d58745ec3c2a28b5e35b7c91
jda447 <user.email>
2022-09-18 21:43:55 +0100

    config env edit

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,8 +4,8 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'GroupomaniaDB',
-    host: '127.0.0.1',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'postgres'
   }
 };
