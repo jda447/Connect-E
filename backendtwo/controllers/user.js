fb04c4dd014f5054e6f8d2798a9b2edcd9c8452f
jda447 <user.email>
2022-10-15 23:13:52 +0100

    user login

--- backendtwo/controllers/user.js
+++ backendtwo/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then(
@@ -32,27 +32,27 @@ exports.signup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email }).then(
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(
     (user) => {
       if (!user) {
         return res.status(401).json({
-          error: new Error('User not found!')
+          error: ('User not found!')
         })
       }
       bcrypt.compare(req.body.password, user.password).then(
         (valid) => {
           if (!valid) {
             return res.status(401).json({
-              error: new Error('Incorrect password!')
+              error: ('Incorrect password!')
             })
           }
-          const token = jwt.sign(
-            { userId: user._id },
-            'RANDOM_TOKEN_SECRET',
-            { expiresIn: '24h' })
           res.status(200).json({
             userId: user._id,
-            token
+            token: 'token'
           })
         }
       ).catch(
