ff55aa8e333aaf394c905883e0bad8af5fe4795d
jda447 <user.email>
2023-01-01 23:04:13 +0000

    favicon & nav toggle

--- groupomania/src/components/HomeNav.vue
+++ groupomania/src/components/HomeNav.vue
@@ -2,7 +2,12 @@
   <div class="navbar">
     <img class="col-4 col-md-3 col-lg-2 mt-1 ms-3" src="@/assets/group-logo-nav.png"/>
     <div class="logos">
-      <div class="dot me-1"></div><span id="online">Online</span>
+      <button 
+        @click="toggle = !toggle" 
+        class="btn online">
+        <span :class="{green: !toggle}">Online |</span>
+        <span :class="{green: toggle}">| Offline</span>
+      </button>
     <router-link to="/profile" class="messagesNavBtn mx-4">
       <font-awesome-icon :icon="['fas', 'user-large']" />
     </router-link>
@@ -22,6 +27,11 @@ export default {
 	name: 'ProfileNav',
     components: {
     SignOutModal
+  },
+  data() {
+    return {
+      toggle: false
+    };
   }
 }
 </script>
@@ -50,16 +60,23 @@ export default {
 hr {
   margin: 0
 }
-.dot {
-  height: 0.8rem;
-  width: 0.8rem;
-  background-color: #08b708;
-  border-radius: 50%;
-  display: inline-block;
+
+.online {
+  font-size: 85%;
+  padding: 2px 4px;
+  color: white;
+  border: solid 1px #0d3b66;
+    &:focus {
+      outline:none;
+      box-shadow: none;
+    }
 }
 
-#online {
-  color: #08b708;
-  font-size: 85%;
+.green {
+  color: #0d3b66;
+}
+
+.btn {
+  margin: 10px;
 }
 </style>
