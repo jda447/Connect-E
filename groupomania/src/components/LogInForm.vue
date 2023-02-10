8ed1733b5795d4713ce82eeac4410f571715494f
jda447 <user.email>
2023-02-10 02:41:18 +0000

    showPassword btn sign

--- groupomania/src/components/LogInForm.vue
+++ groupomania/src/components/LogInForm.vue
@@ -1,46 +1,44 @@
 <template>
   <div id="app">
-    <form id="login-form" class="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto">
+    <form id="login-form" class="col-10 col-sm-6 col-md-5 col-lg-5 mx-auto">
       <h1 class="text-center mt-4 mb-4">Log-in</h1>
-        <div class="form-wrap container">
-          <div v-if="!submitted">
-            <div class="form-group">
-              <label for="email" class="mb-1">Email address</label>
-              <input placeholder="Please enter your email address"
-                v-model="email" 
-                v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlurred}"
-                v-on:blur="emailBlurred = true">
-              <div class="invalid-feedback">A valid email address is required (eg. valid@email.com)</div>
-              <div id="err" class="mt-1"></div>
-            </div>
-            <div class="form-group mt-3">
-              <label for="password" class="mb-1">Password
-                <btn class="eyeButton ms-2" @click.prevent="toggleShow">
-                  <font-awesome-icon :icon="['falight', 'fa-eye']" />
-                </btn>
-              </label>
-                <div class="passwordControl">
-                  <div class="control">
-                  <input v-if="showPassword" type="text" class="input"
-                    v-model="password" 
-                    v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
-                    v-on:blur="passwordBlurred = true"
-                    placeholder="Please enter your password" />
-                
-                  <input v-else type="password" class="input"
-                  v-model="password" 
-                  v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
-                  v-on:blur="passwordBlurred = true"
-                  placeholder="Please enter your password">
-                  </div>
-                </div>
+      <div class="form-wrap container">
+        <div v-if="!submitted">
+          <div class="form-group mb-3">
+            <label for="email" class="mb-1 ms-2 fw-bold">Email address</label>
+            <input placeholder="Please enter your email address"
+              v-model="email" 
+              v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlurred}"
+              v-on:blur="emailBlurred = true">
+            <div class="invalid-feedback">A valid email address is required (eg. valid@email.com)</div>
+            <div id="err" class="mt-1"></div>
+          </div>
 
-                <h6 class="mt-1">Must contain 6-20 characters with at least one number, one uppercase & one lowercase letter</h6>
-              <div class="invalid-feedback">A valid password is required</div>
-            </div>
-            <div class="form-group text-center m-4">
-              <a type="submit" v-on:click.prevent="submit" class="btn btn-lg btnLogInSubmit shadow-none">Submit</a>
-            </div>    
+          <label for="password" class="mb-1 mt-2 ms-2 fw-bold">Password</label>
+          <div class="input-group mb-3">
+            <input v-if="showPassword" type="text" class="input"
+              v-model="password" 
+              v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
+              v-on:blur="passwordBlurred = true"
+              placeholder="Please enter your password" />
+              
+              <input v-else type="password" class="input"
+              v-model="password" 
+              v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
+              v-on:blur="passwordBlurred = true"
+              placeholder="Please enter your password" />
+                  
+            <span class="input-group-text eyeIcon">
+              <btn class="eyeButton" @click.prevent="toggleShow">
+                <font-awesome-icon :icon="['falight', 'fa-eye']" />
+              </btn>
+            </span>
+          </div>
+          <h6 class="mt-1 ms-2">Must contain 6-20 characters with at least one number, one uppercase & one lowercase letter</h6>
+          <div class="invalid-feedback">A valid password is required</div>
+          <div class="form-group text-center m-4">
+            <a type="submit" v-on:click.prevent="submit" class="btn btn-lg btnLogInSubmit shadow-none">Submit</a>
+          </div>    
         </div>
       </div>
     </form>
@@ -159,9 +157,14 @@ h1 {
 }
 
 .eyeButton {
+  color: #0d3b66;
   &:hover {
     color: green;
   }
+}
+
+.eyeIcon {
+  background-color: #f5f5f5
 }
 
 #err {
--- groupomania/src/components/SignUpForm.vue
+++ groupomania/src/components/SignUpForm.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="app">
-    <form id="signup-form" class="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto">
-      <h1 class="text-center mt-4 mb-3">Sign-up</h1>
-        <div class="form-wrap container">
-          <div v-if="!submitted">
-            <div class="form-group">
-              <label for="email" class="mb-1">Email address</label>
-              <input placeholder="Please enter your email address"
-                v-model="email" 
-                v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlurred}"
-                v-on:blur="emailBlurred = true">
-              <div class="invalid-feedback">A valid email address is required (eg. valid@email.com)</div>
-              <div id="err" class="mt-1"></div>
-            </div>
-            <div class="form-group mt-3">
-              <label for="password" class="mb-1">Password</label>
-              <input placeholder="Please choose a valid password"
-                v-model="password" 
-                v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
-                v-on:blur="passwordBlurred = true">
-                <h6 class="mt-1">Must contain 6-20 characters with at least one number, one uppercase & one lowercase letter</h6>
-              <div class="invalid-feedback">A valid password is required</div>
-            </div>           
-            <div class="form-group text-center m-4">
-              <a type="submit" href="#" v-on:click.stop.prevent="submit" class="btn btn-lg btnSignInSubmit shadow-none">Submit</a>
-            </div>    
+    <form id="login-form" class="col-10 col-sm-6 col-md-5 col-lg-5 mx-auto">
+      <h1 class="text-center mt-4 mb-4">Sign-in</h1>
+      <div class="form-wrap container">
+        <div v-if="!submitted">
+          <div class="form-group mb-3">
+            <label for="email" class="mb-1 ms-2 fw-bold">Email address</label>
+            <input placeholder="Please enter your email address"
+              v-model="email" 
+              v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlurred}"
+              v-on:blur="emailBlurred = true">
+            <div class="invalid-feedback">A valid email address is required (eg. valid@email.com)</div>
+            <div id="err" class="mt-1"></div>
           </div>
+
+          <label for="password" class="mb-1 mt-2 ms-2 fw-bold">Password</label>
+          <div class="input-group mb-3">
+            <input v-if="showPassword" type="text" class="input"
+              v-model="password" 
+              v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
+              v-on:blur="passwordBlurred = true"
+              placeholder="Please enter your password" />
+              
+              <input v-else type="password" class="input"
+              v-model="password" 
+              v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlurred}"
+              v-on:blur="passwordBlurred = true"
+              placeholder="Please enter your password" />
+                  
+            <span class="input-group-text eyeIcon">
+              <btn class="eyeButton" @click.prevent="toggleShow">
+                <font-awesome-icon :icon="['falight', 'fa-eye']" />
+              </btn>
+            </span>
+          </div>
+          <h6 class="mt-1 ms-2">Must contain 6-20 characters with at least one number, one uppercase & one lowercase letter</h6>
+          <div class="invalid-feedback">A valid password is required</div>
+          <div class="form-group text-center m-4">
+            <a type="submit" v-on:click.prevent="submit" class="btn btn-lg btnSignInSubmit shadow-none">Submit</a>
+          </div>    
         </div>
+      </div>
     </form>
   </div>
 </template>
@@ -42,13 +56,22 @@ export default {
       passwordBlurred : false,
       emailValid : false,
       passwordValid : false,
-      submitted : false
+      submitted : false,
+      showPassword: false
     }
   },
   mounted: function() {
     this.clearStorage()
   },
+  computed: {
+    buttonLabel() {
+      return (this.showPassword) ? "Hide" : "Show";
+    }
+  },
   methods : {
+    toggleShow() {
+      this.showPassword = !this.showPassword;
+    },
     clearStorage: function() {
     sessionStorage.clear()
     },
@@ -131,6 +154,17 @@ export default {
 
 h1 {
   font-family: Pacifico;
+}
+
+.eyeButton {
+  color: #0d3b66;
+  &:hover {
+    color: green;
+  }
+}
+
+.eyeIcon {
+  background-color: #f5f5f5
 }
 
 #err {
